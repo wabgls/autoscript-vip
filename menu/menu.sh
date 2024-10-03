ff701d3aa59e4270855e9fae3522062d5eeb450f
wabgls <wamine@bogelstore.pw>
2024-10-03 15:19:00 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -290,7 +290,7 @@ clear
 clear && clear && clear
 clear;clear;clear
 echo -e " $COLOR1╭══════════════════════════════════════════════════════════╮${NC}"
-echo -e " $COLOR1│${NC} ${COLBG1}             ${WH} •  BOGEL STORES  PREMIUM •               ${NC} $COLOR1│ $NC"
+echo -e " $COLOR1│${NC} ${COLBG1}              ${WH} •  BOGEL STORES  PREMIUM  •               ${NC} $COLOR1│ $NC"
 echo -e " $COLOR1╰══════════════════════════════════════════════════════════╯${NC}"
 echo -e " $COLOR1╭══════════════════════════════════════════════════════════╮${NC}"
 echo -e " $COLOR1│$NC${WH} ❈ OS            ${COLOR1}: ${WH}$MODEL2${NC}"
